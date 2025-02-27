a6b115f24128bf467b4c19f4a92b922c0a2bcfd6
Benjamin Nolan <benjamin.nolan@slalom.com>
2025-02-27 11:16:12 +1100

    foo

--- app.py
+++ app.py
@@ -18,48 +18,50 @@ def user():
 @app.route('/')
 def mytemplate():
    #Create a template
-   load_dotenv()   
-   strTemplate = "<form action=""/action_page.php"">" 
-   strTemplate = strTemplate + "<table>{% for dbfield in dbfield_list %}"
-   strTemplate = strTemplate + "<tr> <td> <label for='fname'>{{dbfield}}</label> </td>"
-   strTemplate = strTemplate + "<td> <input type=""text"" id={{dbfield}} name={{dbfield}}></td></tr>"
-   strTemplate = strTemplate + "{% endfor %} <table>"
-   strTemplate = strTemplate + "<input type=""submit"" value=""Submit Form"">"
-   strTemplate = strTemplate + "</form>"
-   #Finsih Create a template finished
+   load_dotenv()  
+   text = 'foo'
+   return text 
+   # strTemplate = "<form action=""/action_page.php"">" 
+   # strTemplate = strTemplate + "<table>{% for dbfield in dbfield_list %}"
+   # strTemplate = strTemplate + "<tr> <td> <label for='fname'>{{dbfield}}</label> </td>"
+   # strTemplate = strTemplate + "<td> <input type=""text"" id={{dbfield}} name={{dbfield}}></td></tr>"
+   # strTemplate = strTemplate + "{% endfor %} <table>"
+   # strTemplate = strTemplate + "<input type=""submit"" value=""Submit Form"">"
+   # strTemplate = strTemplate + "</form>"
+   # #Finsih Create a template finished
     
-   # Create a DB Connection with postgres db
-   db_name = os.getenv('db_name')
-   user_name = os.getenv('user_name')
-   port = os.getenv('port')
-   password = os.getenv('password')
-   host = os.getenv('host')
-   pg_db_url = os.getenv('PG_DB_URL')
-   print (pg_db_url)
-   #connection = psycopg2.connect(database=db_name, user=user_name, password=password, host=host, port=port)
-   connection = psycopg2.connect(pg_db_url,sslmode='require')
-   # Finish create DB Connection
+   # # Create a DB Connection with postgres db
+   # db_name = os.getenv('db_name')
+   # user_name = os.getenv('user_name')
+   # port = os.getenv('port')
+   # password = os.getenv('password')
+   # host = os.getenv('host')
+   # pg_db_url = os.getenv('PG_DB_URL')
+   # print (pg_db_url)
+   # #connection = psycopg2.connect(database=db_name, user=user_name, password=password, host=host, port=port)
+   # connection = psycopg2.connect(pg_db_url,sslmode='require')
+   # # Finish create DB Connection
 
-   #Create DB field list
-   cursor = connection.cursor()
-   #cursor.execute("SELECT Column_name, data_type,udt_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = 'salesforce' AND table_name   = 'contact'")
-   cursor.execute("SELECT Column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = 'salesforce' AND table_name   = 'contact' And Data_type <> 'timestamp without time zone' ")
-   #print (list(cursor))
-   fieldlist = list(cursor)
-   # fieldlist = [field.replace('(','') for field in fieldlist ]
-   # print (fieldlist)
-   #
+   # #Create DB field list
+   # cursor = connection.cursor()
+   # #cursor.execute("SELECT Column_name, data_type,udt_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = 'salesforce' AND table_name   = 'contact'")
+   # cursor.execute("SELECT Column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = 'salesforce' AND table_name   = 'contact' And Data_type <> 'timestamp without time zone' ")
+   # #print (list(cursor))
+   # fieldlist = list(cursor)
+   # # fieldlist = [field.replace('(','') for field in fieldlist ]
+   # # print (fieldlist)
+   # #
 
-   #t=Template("<Title> {{ title }} </Title><body> {{ content }} </body>")
-   t=Template(strTemplate)
-   #dict = {"title": "First page", "content": "This is the first page"}
-   dbfield_list = []
-   for p in fieldlist:
-      #print (p)
+   # #t=Template("<Title> {{ title }} </Title><body> {{ content }} </body>")
+   # t=Template(strTemplate)
+   # #dict = {"title": "First page", "content": "This is the first page"}
+   # dbfield_list = []
+   # for p in fieldlist:
+   #    #print (p)
       
-      dbfield_list.append(p)
+   #    dbfield_list.append(p)
 
-   return t.render(dbfield_list=dbfield_list)
+   # return t.render(dbfield_list=dbfield_list)
 if __name__ == '__main__':
    app.run(debug=TRUE)
    app.run()
